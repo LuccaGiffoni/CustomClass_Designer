1c05c75cfc906d74292d163937fff9823be3e8fe
Lucca Giffoni <luccagiffoni@outlook.com>
2021-04-29 12:43:51 -0300

    Update UsingTheClass.cs

--- CustomClass_Designer/UsingTheClass.cs
+++ CustomClass_Designer/UsingTheClass.cs
@@ -12,17 +12,20 @@ namespace UsingMyClass
             // Chosse a name for your custom store
             string MyStoreName = "MyStore";
 
-            // Variables for quantity
-            int userResponse;
-            int limit = 0;
+            // Variables for quantity -- all start with zero to not 'exist' if not selected by the user
             int laptopQuantity = 0;
             int phoneQuantity = 0;
             int mouseQuantity = 0;
+
+            // Variables for the user's response
+            int userResponse;
+            int limit = 0;
 
             #endregion
 
             #region Header
 
+            // You can change the name of your "store"
             Console.WriteLine("Welcome to " + MyStoreName);
             Console.WriteLine("-------------------------------------------------------------------------------------");
             Console.WriteLine("Catalog:");
@@ -30,13 +33,16 @@ namespace UsingMyClass
             Console.WriteLine("[2] M1.0WN Phone -- A24 Processor + 8 GB of RAM +  33643 MB of Memory | Price: 899 U$");
             Console.WriteLine("[3] M1.0WN Mouse -- KB24/8 Sensor | Price: 49 U$");
             Console.WriteLine("-------------------------------------------------------------------------------------");
-            Console.WriteLine("Choose what you want (type the number of the item -- 1, 2 or 3): ");
-            userResponse = Convert.ToInt32(Console.ReadLine());
 
             #endregion
 
             #region UserChoosing
 
+            // Getting the user response
+            Console.WriteLine("Choose what you want (type the number of the item -- 1, 2 or 3): ");
+            userResponse = Convert.ToInt32(Console.ReadLine());
+
+            // Making the user select everything he wants how many time he want it
             do
             {
                 if (userResponse == 1)
